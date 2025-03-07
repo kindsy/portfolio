71c07d4a033373135e8a0fc8068c25871c1a6769
kailind <klin0018@student.monash.edu>
2025-03-07 14:43:25 +1100

    New animated face!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import face from './face.svg';
+// import face from './face.svg';
+import face from './ani-face.gif';
 import './App.css';
+
+const Year = new Date().getFullYear() +"";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={face} className="App-face" alt="face"/>
+        <a href="https://www.linkedin.com/in/kailindsay/" target="_blank" rel="noreferrer">
+          <img src={face} className="App-face" alt="face"/>
+        </a>
         <p>
-          A narrative portfolio.
+          a narrative portfolio.
         </p>
       </header>
+      <main className="App-main">
+        <p>
+          UX/UI is my passion.
+        </p>
+      </main>
+      <footer className="App-footer">
+        <p>
+          © {Year} kai lindsay. all rights.
+        </p>
+      </footer>
     </div>
   );
 }
